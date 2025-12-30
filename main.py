4547cfdd0bbbd3551d9578b6cfd6a548705e6327
gooberf <liamniday3@gmail.com>
2025-12-30 16:42:59 -0500

    meow 3

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ def get_book_text(file_path:str):
         return contents
 
 def word_count(text:str):
-    
+    num_words = 0
+    words = text.split()
+    for word in words: num_words += 1
+    return num_words
 
 def main(file_path:str):
-    print(get_book_text(file_path))
+    num_words = word_count(get_book_text(file_path))
+    print(f'Found {num_words} total words')
 
 main('books/frankenstein.txt')
