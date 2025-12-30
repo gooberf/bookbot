06e0e7111229bd7ff4818e14811ed32332fd9af8
gooberf <liamniday3@gmail.com>
2025-12-30 16:37:30 -0500

    a

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ def get_book_text(file_path:str):
     with open(file_path) as f:
         contents = f.read()
         return contents
+
+def word_count(text:str):
+    
+
+def main(file_path:str):
+    print(get_book_text(file_path))
+
+main('books/frankenstein.txt')
